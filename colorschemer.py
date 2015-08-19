6a7de07dcd9f9f3c0ce799e89b8b384a8d17b6f5
Sanjeev Pandey <yednap@gmail.com>
2015-08-18 17:03:37 -0700

    update script to accept url as argument, add iteration number to console output

--- colorschemer.py
+++ colorschemer.py
@@ -8,12 +8,12 @@ def getcolors(inp):
 def hextorgb(htmlcol):
     return tuple([int(''.join(i),16) for i in zip(htmlcol[::2],htmlcol[1::2])])
 
-def colorsteal(lis):
+def colorgrab(lis,url):
     browser = webdriver.Firefox()
     for i in range(20):
-        browser.get("https://coolors.co/app")
+        browser.get(url)
         delay = randint(4,15)
-        print("waiting {}sec".format(delay))
+        print("{}:\twaiting {}sec".format(i,delay))
         os.system("sleep {}".format(delay))
         lis.append(getcolors(browser.current_url))
     return lis
